e11911886c1a2e22820dca796c5a1e7ff801822f
Robert Sun <107655677+rsun19@users.noreply.github.com>
2024-07-29 17:26:08 -0400

    added connection type url

---
+++ frontend/src/pages/connectionTypes/connectionTypes.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable arrow-body-style */
+import React from 'react';
+
+const ConnectionTypes: React.FC = () => {
+  return (
+    <div>
+      <div>lol</div>
+    </div>
+  );
+};
+
+export default ConnectionTypes;
